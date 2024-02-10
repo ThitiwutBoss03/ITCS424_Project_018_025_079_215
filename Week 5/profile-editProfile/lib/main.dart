4f2ccbda1049c4c48c87101c86a4bcf5d8f961bb
Thitiwut Boss <thitiwutmcpeakz@gmail.com>
2024-02-10 20:37:19 +0700

    week 5 homework

---
+++ Week 5/profile-editProfile/lib/main.dart
@@ -0,0 +1,354 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProfilePage(),
+    );
+  }
+}
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  int _currentIndex = 4; // Setting current page to Profile Page
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color: Color(0xFF27346A),
+            height: 70, // Adjust the height as needed
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/Logo.png', // Replace with your actual logo path
+                    fit: BoxFit.contain,
+                    height: 60, // Adjust the height of the logo as needed
+                  ),
+                ),
+                Expanded(
+                  child:
+                      Container(), // Empty container to expand and cover the width
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 16, top: 16, bottom: 8),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Profile',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage('assets/profile_picture.png'),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Somchai',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'staff@gmail.com',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    SizedBox(height: 30),
+                    buildProfileOption('Edit Profile', Icons.edit, () {
+                      // Add navigation or functionality for Edit Profile
+                    }),
+                    buildProfileOption('Notifications', Icons.notifications,
+                        () {
+                      // Add navigation or functionality for Notifications
+                    }),
+                    buildProfileOption('About', Icons.info, () {
+                      // Add navigation or functionality for About
+                    }),
+                    buildProfileOption('Security', Icons.security, () {
+                      // Add navigation or functionality for Security
+                    }),
+                    buildProfileOption('Language', Icons.language, () {
+                      // Add navigation or functionality for Language
+                    }),
+                    buildProfileOption('Sign Out', Icons.exit_to_app, () {
+                      // Add functionality for Sign Out
+                    }, textColor: Colors.red, iconColor: Colors.red),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Bookmark',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.announcement),
+            label: 'Announcement',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProfileOption(String title, IconData icon, VoidCallback? onTap,
+      {Color? textColor, Color? iconColor}) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(
+          color: textColor,
+        ),
+      ),
+      leading: Icon(
+        icon,
+        color: iconColor,
+      ),
+      onTap: onTap != null
+          ? () {
+              if (title == 'Edit Profile') {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => EditProfilePage()),
+                );
+              } else {
+                onTap();
+              }
+            }
+          : null,
+    );
+  }
+}
+
+class EditProfilePage extends StatelessWidget {
+  // Controller for text fields
+  final TextEditingController firstNameController = TextEditingController(text: 'Somchai');
+  final TextEditingController lastNameController = TextEditingController(text: 'Jaidee');
+  final TextEditingController emailController = TextEditingController(text: 'staff@gmail.com');
+  final TextEditingController contactNumberController = TextEditingController(text: '0812345678');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // Header
+          Container(
+            color: Color(0xFF27346A),
+            height: 70, // Adjust the height as needed
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/Logo.png', // Replace with your actual logo path
+                    fit: BoxFit.contain,
+                    height: 60, // Adjust the height of the logo as needed
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Edit Profile Header
+          Container(
+            height: 70,
+            child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Text(
+                  ' Edit Profile',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    // Profile Picture
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage('assets/profile_picture.png'),
+                    ),
+                    SizedBox(height: 10),
+                    // Name and Email
+                    Text(
+                      'Somchai',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'staff@gmail.com',
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    SizedBox(height: 20),
+                    // Editable Fields
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: firstNameController,
+                                  decoration: InputDecoration(
+                                    hintText: 'First Name',
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: TextField(
+                                  controller: lastNameController,
+                                  decoration: InputDecoration(
+                                    hintText: 'Last Name',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          TextField(
+                            controller: emailController,
+                            decoration: InputDecoration(
+                              hintText: 'Enter your email',
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextField(
+                            controller: contactNumberController,
+                            decoration: InputDecoration(
+                              hintText: 'Enter your contact number',
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          // Confirm Changes Button
+                          Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Add functionality to confirm changes
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xFF27346A),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                padding: EdgeInsets.symmetric(vertical: 15),
+                              ),
+                              child: Text(
+                                'Confirm Changes',
+                                style: TextStyle(fontSize: 18, color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 4,
+        onTap: (index) {
+          // Handle navigation to other pages
+        },
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Bookmark',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.announcement),
+            label: 'Announcement',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notification',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
