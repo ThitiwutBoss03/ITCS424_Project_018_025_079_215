3acd1375b504a7b8586a00fbdc83ac8704011c33
Burit Sihabut <bestburit32121@gmail.com>
2024-02-13 11:03:32 +0700

    update main.dart and pdf file (description)

--- Week 5/lib/main.dart
+++ Week 5/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
