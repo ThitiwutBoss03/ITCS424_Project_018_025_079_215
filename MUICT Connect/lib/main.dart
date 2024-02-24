b4c067a34cffa5f25917e85cadd391d7b7de3c7f
Ramita Deeprom <62588305+RamitaDeeprom@users.noreply.github.com>
2024-02-25 00:45:41 +0700

    update dependencies + environment + push calendar test file

--- MUICT Connect/lib/main.dart
+++ MUICT Connect/lib/main.dart
@@ -1,238 +1,401 @@
-// import 'package:flutter/material.dart';
-import 'package:flutter/material.dart' hide VoidCallback;
-import 'package:table_calendar/table_calendar.dart';
-//import 'dart:html';
+// <head>
+// <meta name="google-signin-client_id"
+// content="YOUR_CLIENT_ID_HERE" />
+// </head>
+// OAuth 2.0 Client IDs
+// flutter run -d chrome --web-port 5555
 
-import 'announcement.dart';
-import 'profile.dart';
-import 'bookmark.dart';
-import 'notification.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:oauth2_client/google_oauth2_client.dart';
+import 'package:googleapis/calendar/v3.dart' as GoogleAPI;
+import 'package:http/io_client.dart' show IOClient, IOStreamedResponse;
+import 'package:http/http.dart' show BaseRequest, Response;
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: DashboardApp(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
-// Dashboard
-class DashboardApp extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
-  _DashboardState createState() => _DashboardState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _DashboardState extends State<DashboardApp> {
-  int _currentIndex = 0;
-  
+class _MyHomePageState extends State<MyHomePage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    clientId: '205327508421-uchcrj6i0mgj9kcbpd0rj43898gkcgd1.apps.googleusercontent.com',
+    // apiKey: 'AIzaSyCfIqv2bu2nXrPCe3pC9VZnPIL_5ghp-Kw',
+    scopes: <String>[GoogleAPI.CalendarApi.calendarScope],
+  );
+
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        //getGoogleEventsData();
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
+  Future<List<GoogleAPI.Event>> getGoogleEventsData() async {
+    final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+    final GoogleAPIClient httpClient =
+        GoogleAPIClient(await googleUser!.authHeaders);
+
+    final GoogleAPI.CalendarApi calendarApi = GoogleAPI.CalendarApi(httpClient);
+    final GoogleAPI.Events calEvents = await calendarApi.events.list(
+      "primary",
+    );
+    final List<GoogleAPI.Event> appointments = <GoogleAPI.Event>[];
+    if (calEvents.items != null) {
+      for (int i = 0; i < calEvents.items!.length; i++) {
+        final GoogleAPI.Event event = calEvents.items![i];
+        if (event.start == null) {
+          continue;
+        }
+        appointments.add(event);
+      }
+    }
+
+    return appointments;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            color: Color(0xFF27346A),
-            height: 70, // Adjust the height as needed
-            child: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Image.asset(
-                    'assets/Logo.png', // Replace with your actual logo path
-                    fit: BoxFit.contain,
-                    height: 60, // Adjust the height of the logo as needed
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // content box
-          Expanded(
-            child: ListView(
-              children: [
-                // margin: new EdgeInsets.symmetric(horizontal: 20.0),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  child: Column(
-                    children: [
-                      Text(
-                        'Calendar',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
-                    // calendar
-                      TableCalendar(
-                      firstDay: DateTime.utc(2020, 1, 1),
-                      lastDay: DateTime.utc(2030, 3, 14),
-                      focusedDay: DateTime.now(),
-                    ), //https://pub.dev/packages/table_calendar
-                    //end calendar
-                    ],
-                  ),
-                ),
-                // Display multiple boxes
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  child: Text(
-                    'Activities',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                  ),
-                ),
-                BoxWidgetWithImageOnLeft(
-                  imageUrl: 'assets/dash.jpg',
-                  title: 'ICT Announcement 1',
-                  description: 'ho ho ho ho ho ho ho',
-                  date: 'Feb 2, 2022',
-                  category: 'Registration',
-                ),
-                // Add more BoxWidget instances as needed
-              ],
-            ),
-          ),
-        ],
+      appBar: AppBar(
+        title: const Text('Event Calendar'),
       ),
-      // Nav bar
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-            if (index == 0) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DashboardApp(),
-                ),
-              );
-            }
-            else if (index == 1) { // Assuming 'Announcement' is at index 2
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => BookmarkPage(),
-                ),
-              );
-            }
-            else if (index == 2) { // Assuming 'Announcement' is at index 2
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Announcement(),
-                ),
-              );
-            }
-            else if (index == 3) { // Assuming 'Announcement' is at index 2
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => NotificationPage(),
-                ),
-              );
-            }
-            else if (index == 4) { // Assuming 'Announcement' is at index 2
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ProfilePage(),
-                ),
-              );
-            }
-          },
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bookmark),
-            label: 'Bookmark',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.announcement),
-            label: 'Announcement',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Notification',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+      body: FutureBuilder(
+        future: getGoogleEventsData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Stack(
+            children: [
+              SfCalendar(
+                view: CalendarView.month,
+                initialDisplayDate: DateTime(2020, 7, 15, 9, 0, 0),
+                dataSource: GoogleDataSource(events: snapshot.data),
+                monthViewSettings: const MonthViewSettings(
+                    appointmentDisplayMode:
+                        MonthAppointmentDisplayMode.appointment),
+              ),
+              snapshot.data != null
+                  ? Container()
+                  : const Center(
+                      child: CircularProgressIndicator(),
+                    )
+            ],
+          );
+        },
       ),
     );
   }
-}
-
-// Bow on the left
-class BoxWidgetWithImageOnLeft extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String description;
-  final String date;
-  final String category;
-
-  BoxWidgetWithImageOnLeft({
-    required this.imageUrl,
-    required this.title,
-    required this.description,
-    required this.date,
-    required this.category,
-  });
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Image on the left
-          Image.asset(
-            imageUrl,
-            width: 250,
-            height: 350,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(width: 10), // Add some space between the image and text
-          // Text and other details on the right
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  description,
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  'Date: $date',
-                  style: TextStyle(fontSize: 14),
-                ),
-                Text(
-                  'Category: $category',
-                  style: TextStyle(fontSize: 14),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+  void dispose() {
+    if (_googleSignIn.currentUser != null) {
+      _googleSignIn.disconnect();
+      _googleSignIn.signOut();
+    }
+
+    super.dispose();
   }
 }
-// end Box on the left
+
+class GoogleDataSource extends CalendarDataSource {
+  GoogleDataSource({required List<GoogleAPI.Event>? events}) {
+    appointments = events;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.start?.date ?? event.start!.dateTime!.toLocal();
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].start.date != null;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.endTimeUnspecified != null && event.endTimeUnspecified!
+        ? (event.start?.date ?? event.start!.dateTime!.toLocal())
+        : (event.end?.date != null
+            ? event.end!.date!.add(const Duration(days: -1))
+            : event.end!.dateTime!.toLocal());
+  }
+
+  @override
+  String getLocation(int index) {
+    return appointments![index].location ?? '';
+  }
+
+  @override
+  String getNotes(int index) {
+    return appointments![index].description ?? '';
+  }
+
+  @override
+  String getSubject(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.summary == null || event.summary!.isEmpty
+        ? 'No Title'
+        : event.summary!;
+  }
+}
+
+class GoogleAPIClient extends IOClient {
+  final Map<String, String> _headers;
+
+  GoogleAPIClient(this._headers) : super();
+
+  @override
+  Future<IOStreamedResponse> send(BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<Response> head(Uri url, {Map<String, String>? headers}) =>
+      super.head(url,
+          headers: (headers != null ? (headers..addAll(_headers)) : headers));
+}
+
+// import 'package:flutter/material.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:googleapis/calendar/v3.dart' as GoogleAPI;
+// import 'package:oauth2_client/google_oauth2_client.dart';
+// import 'package:http/io_client.dart' show IOClient, IOStreamedResponse;
+// import 'package:http/http.dart' show BaseRequest, Response;
+// import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   final GoogleSignIn _googleSignIn = GoogleSignIn(
+//     clientId: '205327508421-uchcrj6i0mgj9kcbpd0rj43898gkcgd1.apps.googleusercontent.com',
+//     scopes: <String>[
+//       'email',
+//       GoogleAPI.CalendarApi.calendarScope,
+//     ],
+//   );
+
+//   GoogleSignInAccount? _currentUser;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+//       setState(() {
+//         _currentUser = account;
+//       });
+//     });
+//     _googleSignIn.signInSilently().catchError((error) {
+//       print('Error signing in silently: $error');
+//     });
+//   }
+
+//   Future<void> _handleSignIn() async {
+//     try {
+//       await _googleSignIn.signIn();
+//     } catch (error) {
+//       print('Error signing in: $error');
+//     }
+//   }
+
+
+//   Future<List<GoogleAPI.Event>> getGoogleEventsData() async {
+//     try {
+//       if (_currentUser == null) {
+//         throw Exception('User is not signed in.');
+//       }
+
+//       final GoogleAPIClient httpClient =
+//           GoogleAPIClient(await _currentUser!.authHeaders);
+
+//       final GoogleAPI.CalendarApi calendarApi =
+//           GoogleAPI.CalendarApi(httpClient);
+//       final GoogleAPI.Events calEvents = await calendarApi.events.list(
+//         "primary",
+//       );
+
+//       return calEvents.items ?? [];
+//     } catch (e) {
+//       print('Error fetching events: $e');
+//       return []; // Return an empty list in case of error
+//     }
+//   }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Event Calendar'),
+//       ),
+//       body: FutureBuilder<List<GoogleAPI.Event>>(
+//         future: getGoogleEventsData(),
+//         builder: (BuildContext context,
+//             AsyncSnapshot<List<GoogleAPI.Event>> snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return const Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           } else if (snapshot.hasError) {
+//             return Center(
+//               child: Text('Error: ${snapshot.error}'),
+//             );
+//           } else {
+//             return _currentUser != null
+//                 ? SfCalendar(
+//                     view: CalendarView.month,
+//                     initialDisplayDate: DateTime(2020, 7, 15, 9, 0, 0),
+//                     dataSource: GoogleDataSource(events: snapshot.data),
+//                     monthViewSettings: const MonthViewSettings(
+//                         appointmentDisplayMode:
+//                             MonthAppointmentDisplayMode.appointment),
+//                   )
+//                 : Center(
+//                     child: ElevatedButton(
+//                       onPressed: _handleSignIn,
+//                       child: Text('Sign in with Google'),
+//                     ),
+//                   );
+//           }
+//         },
+//       ),
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     if (_googleSignIn.currentUser != null) {
+//       _googleSignIn.disconnect();
+//     }
+//     super.dispose();
+//   }
+// }
+
+// class GoogleDataSource extends CalendarDataSource {
+//   GoogleDataSource({required List<GoogleAPI.Event>? events}) {
+//     appointments = events;
+//   }
+
+//   @override
+//   DateTime getStartTime(int index) {
+//     final GoogleAPI.Event event = appointments![index];
+//     return event.start?.date ?? event.start!.dateTime!.toLocal();
+//   }
+
+//   @override
+//   bool isAllDay(int index) {
+//     return appointments![index].start.date != null;
+//   }
+
+//   @override
+//   DateTime getEndTime(int index) {
+//     final GoogleAPI.Event event = appointments![index];
+//     return event.endTimeUnspecified != null && event.endTimeUnspecified!
+//         ? (event.start?.date ?? event.start!.dateTime!.toLocal())
+//         : (event.end?.date != null
+//             ? event.end!.date!.add(const Duration(days: -1))
+//             : event.end!.dateTime!.toLocal());
+//   }
+
+//   @override
+//   String getLocation(int index) {
+//     return appointments![index].location ?? '';
+//   }
+
+//   @override
+//   String getNotes(int index) {
+//     return appointments![index].description ?? '';
+//   }
+
+//   @override
+//   String getSubject(int index) {
+//     final GoogleAPI.Event event = appointments![index];
+//     return event.summary == null || event.summary!.isEmpty
+//         ? 'No Title'
+//         : event.summary!;
+//   }
+// }
+
+// class GoogleAPIClient extends IOClient {
+//   final Map<String, String> _headers;
+
+//   GoogleAPIClient(this._headers) : super();
+
+//   @override
+//   Future<IOStreamedResponse> send(BaseRequest request) async {
+//     final Response response = (await super.send(request..headers.addAll(_headers))) as Response;
+
+//     // Convert Response body to Stream<List<int>>
+//     final stream = Stream<List<int>>.fromIterable([response.bodyBytes]);
+
+//     // Create and return IOStreamedResponse
+//     return IOStreamedResponse(
+//       stream,
+//       response.statusCode,
+//       contentLength: response.contentLength,
+//       request: response.request,
+//       headers: response.headers,
+//       reasonPhrase: response.reasonPhrase,
+//     );
+//   }
+
+//   @override
+//   Future<Response> head(Uri url, {Map<String, String>? headers}) =>
+//       super.head(url, headers: (headers != null ? (headers..addAll(_headers)) : headers));
+// }
